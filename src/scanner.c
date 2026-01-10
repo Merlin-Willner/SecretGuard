6f5933336852045d7bb4c5a9c49131597dc0d1c6
Merlin Willner <merlinwillner.programming@gmail.com>
2026-01-10 18:32:06 +0100

    layed out the next steps to get to an MVP (A minimal viable product) and what could be improved after that

---
+++ src/scanner.c
@@ -0,0 +1,14 @@
+
+
+/* Reads every file or standard in stdin line by line
+runs the regex rules on each line, and prints findings when a match is found */
+
+
+
+/* After that the code should work for APIs and Passwords.#
+ The next step would be:
+ 1. Expand for more regex rules
+ to detect more critical security flaws
+ 2. Implement multi threading for performance gains
+
+ */
---
+++ src/walk.c
@@ -0,0 +1,4 @@
+
+
+/* The code in this file should wald through a root path, 
+go to subfolders to the maxdepth and call a callback for every file it finds  */
